db30aab4ccd1fde4b1df26aae562b4623dfb2bca
Nefeli Morales <nefelimorales@MacBook-Air.localdomain>
2018-10-25 12:03:06 -0500

    finishing the design layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from './components/Header.js'
+import 'antd/dist/antd.css';
+import './index.css';
+import { Layout, Menu, Breadcrumb } from 'antd';
+
+const { Header, Content, Footer } = Layout;
 
 
 
 class App extends Component {
   render() {
-    return (
+
+      return(
       <div className="App">
-          <Header/>
+          <Layout>
+              <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+                  <div className="logo" />
+                  <Menu
+                      theme="dark"
+                      mode="horizontal"
+                      defaultSelectedKeys={['2']}
+                      style={{ lineHeight: '64px' }}
+                  >
+                      <Menu.Item key="1">nav 1</Menu.Item>
+                      <Menu.Item key="2">nav 2</Menu.Item>
+                      <Menu.Item key="3">nav 3</Menu.Item>
+                  </Menu>
+              </Header>
+              <Content style={{ padding: '0 50px', marginTop: 64 }}>
+                  <Breadcrumb style={{ margin: '16px 0' }}>
+                      <Breadcrumb.Item>Home</Breadcrumb.Item>
+                      <Breadcrumb.Item>List</Breadcrumb.Item>
+                      <Breadcrumb.Item>App</Breadcrumb.Item>
+                  </Breadcrumb>
+                  <div style={{ background: '#fff', padding: 24, minHeight: 380 }}>Content</div>
+              </Content>
+              <Footer style={{ textAlign: 'center' }}>
+                  Lima-peru ©2018 Created by Nefeli
+              </Footer>
+          </Layout>
+
       </div>
     );
   }
